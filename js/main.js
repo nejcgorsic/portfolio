e96a010c2f0454b7c5b73b21b7069f088d74c4bd
Nejc Goršič <nejcgorsic1@gmail.com>
2020-02-18 17:40:06 +0100

    Gallery

--- js/main.js
+++ js/main.js
@@ -12,4 +12,11 @@ $(document).ready(function () {
 
         return false;
     })
+
+    $('.project-area .button-group #btn1-active').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type:'image',
+        gallery:{enabled: true}
+    });
 });
